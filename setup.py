5fb8d0a14489feebf2817b8b84c25100f6080d9b
Hobbestigrou <hobbestigrou@erakis.im>
2012-10-21 20:12:39 +0200

    [Core] Setup: Add a setup.py to install the project.

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(
+    name = "pylocwolowitz",
+    packages = ["pylocwolowitz"],
+    version = "0.1",
+    description = "Simple localization for web apps with JSON.",
+    author = "Natal Ngetal",
+    author_email = "hobbestigrou@erakis.im",
+    url = "",
+    download_url = "",
+    keywords = ["i18n", "json"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Linguistic",
+        ],
+    long_description = """\
+Simple localization
+-------------------
+
+Pylocwolowitz is a very simple text localization system, meant to be used by web applications (but can pretty much be used anywhere). Yes, another localization system.
+
+This version requires Python 3 or later.
+"""
+)
