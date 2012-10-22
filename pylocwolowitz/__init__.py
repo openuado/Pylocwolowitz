e74a8193952e918f0f859f1796595db14e59921c
Hobbestigrou <hobbestigrou@erakis.im>
2012-10-23 00:28:46 +0200

    [Core] Init: To be pylint compliant.

--- pylocwolowitz/__init__.py
+++ pylocwolowitz/__init__.py
@@ -5,27 +5,31 @@ import os.path
 from collections import defaultdict
 
 class Pylocwolowitz(object):
+    '''Pylocwolitz  is a very imple text localization system, meant to be used
+    by web applications (but can pretty much be used anywhere). Yes, another
+    localization system.'''
 
     def __init__(self, path):
         self.path = path
         self.locales = defaultdict(dict)
-        self._make_loc()
+        self._find_file()
 
     def _find_file(self):
+        '''Find all json files'''
         listing = glob.glob(os.path.join(self.path, '*.json'))
         for infile in listing:
             self._make_loc(infile)
 
     def _make_loc(self, infile):
-        with open(infile) as f:
-            data = json.load(f)
+        '''Store content of the file in a memory'''
+        with open(infile) as fil:
+            data = json.load(fil)
         for key, value in data.items():
             if isinstance(value, dict):
                 self.locales[key].update(value)
             else:
                 lang = os.path.basename(infile).split('.')[0]
                 self.locales[key].update({lang: value})
-        return
 
     def loc(self, key, lang, values=None):
         '''Return the string key, translated to the requested language (if such
