d16404d2fd7930b8bb1e64f5f85fd792ea067189
Hobbestigrou <hobbestigrou@erakis.im>
2012-10-30 23:11:00 +0100

    [Core] Init: Accept multiple format.

--- pylocwolowitz/__init__.py
+++ pylocwolowitz/__init__.py
@@ -1,31 +1,36 @@
 import glob
-import json
 import re
 import os.path
 from collections import defaultdict
-
 
 class Pylocwolowitz(object):
     '''Pylocwolitz is a very simple text localization system, meant to be used
     by web applications (but can pretty much be used anywhere). Yes, another
     localization system.'''
 
-    def __init__(self, path):
+    def __init__(self, path, format_deserializer='json'):
         self.path = path
+        self.format_deserializer = format_deserializer
         self.locales = defaultdict(dict)
         self._find_file()
 
     def _find_file(self):
         '''Find all json files'''
-        listing = glob.glob(os.path.join(self.path, '*.json'))
+        listing = glob.glob(os.path.join(self.path, '*.' + self.format_deserializer))
         for infile in listing:
             self._make_loc(infile)
 
     def _make_loc(self, infile):
         '''Store content of the file in a memory'''
         lang = os.path.basename(infile).split('.')[0]
-        with open(infile) as fil:
-            data = json.load(fil)
+        if self.format_deserializer == 'json':
+            import json
+            with open(infile) as fil:
+                data = json.load(fil)
+        else:
+            import yaml
+            with open(infile) as fil:
+                data = yaml.load(fil)
         for key, value in data.items():
             if isinstance(value, dict):
                 self.locales[key].update(value)
